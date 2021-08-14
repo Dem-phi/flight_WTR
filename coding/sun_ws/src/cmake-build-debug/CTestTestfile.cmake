f0729b9061d91c5b49af1b633f3b54f3e07a65d9
Dem-phi <demphi339413361@outlook.com>
2021-08-14 15:21:28 +0800

    2021/8/14 by demphi

--- coding/sun_ws/src/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/demphi/sun_ws/src
-# Build directory: /home/demphi/sun_ws/src/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("main_controller")
